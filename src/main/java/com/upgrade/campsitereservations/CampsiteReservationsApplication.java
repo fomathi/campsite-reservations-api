a529356a46414d3452476b7a0cc0e6d99a288eff
Mathias kodjo agbayi <mathias1073@gmail.com>
2022-05-16 22:47:13 -0400

    Initial commit - project skeleton

---
+++ src/main/java/com/upgrade/campsitereservations/CampsiteReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.upgrade.campsitereservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsiteReservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampsiteReservationsApplication.class, args);
+    }
+
+}
